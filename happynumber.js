5042adaf94a1dcc81dd0a4b647a36bd471f863bb
Roho7 <rohosen2@gmail.com>
2023-08-07 21:21:12 +0530

    ...

--- happynumber.js
+++ happynumber.js
@@ -2,18 +2,27 @@
  * @param {number} n
  * @return {boolean}
  */
+const first = [];
+var flag = 0;
 var isHappy = function (n) {
   let arr = Array.from(String(n), Number);
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     arr[i] = arr[i] ** 2;
     sum = sum + arr[i];
+    if (first.includes(sum)) {
+      flag = 1;
+      return false;
+    }
   }
   if (sum != 1) {
+    first.push(sum);
     isHappy(sum);
-    console.log(sum);
+  }
+  if (flag != 0) {
+    return false;
   } else {
-    console.log("true", sum);
+    return true;
   }
 };
-isHappy(2);
+console.log(isHappy(7));
